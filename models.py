0eb69a72c7ed680257d8c837ad0e51e1b5bde5bd
daewhakangdesign <daewhakangdesign@gmail.com>
2017-07-09 12:19:18 +0100

    First fiddling with models.py
    Aiming to understand the ndb. query functions properly.

--- models.py
+++ models.py
@@ -9,10 +9,16 @@ class Person(ndb.Model):
         event = Event(parent=self.key, event_type=event_type)
         event.put()
 
+    #Below we set up the properties accessible from the google datastore.
     @property
     def event_count(self):
         return Event.query(ancestor=self.key).count()
 
+    #Here we must develop a query filtered for only the current month
+    @property
+    def event_thismonth(self):
+	#not yet working. #return Event.query(ancestor=self.key).filter(Event.name=="DaeWha Kang").count()
+        return Event.query(ancestor=self.key).count(limit=12) #test the property
 
     def reset(self):
         ndb.delete_multi(Event.query(ancestor=self.key).iter(keys_only=True))
